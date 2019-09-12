370e9d3cfd791a62429f9e934646ed15a0ae0126
@japzio <jasper.culong@trustarc.com>
2019-09-12 15:07:24 +0800

    initial commit

---
+++ src/main/java/com/japzio/demologfwdheader/DemologfwdheaderApplication.java
@@ -0,0 +1,13 @@
+package com.japzio.demologfwdheader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemologfwdheaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemologfwdheaderApplication.class, args);
+	}
+
+}
