591f2c0f27ec58d1e18346619c4a86f9a8c60f57
@japzio <jasper.culong@trustarc.com>
2019-09-12 18:22:38 +0800

    :-(

--- src/main/java/com/japzio/demologfwdheader/config/HeaderFilter.java
+++ src/main/java/com/japzio/demologfwdheader/config/HeaderFilter.java
@@ -2,29 +2,35 @@ package com.japzio.demologfwdheader.config;
 
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+@Component
 public class HeaderFilter implements Filter {
 
-  @Value("${logging.header:X-Request-ID}")
-  private String headerName;
-  private MDC mappedDiagnosticContext;
+  @Value("${logging.custom-header:X-Request-ID}")
+  private String extraHeader;
 
   @Override
   public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 
     try {
-      mappedDiagnosticContext.put(headerName, ((HttpServletRequest) servletRequest).getHeader(headerName));
-      mappedDiagnosticContext.put(headerName, ((HttpServletResponse) servletResponse).getHeader(headerName));
+      MDC.put(extraHeader, ((HttpServletRequest)servletRequest).getHeader(extraHeader));
+      ((HttpServletResponse) servletResponse).setHeader(extraHeader, ((HttpServletRequest)servletRequest).getHeader(extraHeader));
       filterChain.doFilter(servletRequest, servletResponse);
     } finally {
-      mappedDiagnosticContext.clear();
+      MDC.clear();
     }
 
   }
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {}
+  @Override
+  public void destroy() {}
 
 }
